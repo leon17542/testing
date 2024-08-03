f7d954130393c5235e44153b588bfdcf129f7cfc
leon17542 <leonsoft.euler@gmail.com>
2024-08-03 21:18:43 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="dark">
 <head>
-  <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-SK1N7CEJHK"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-SK1N7CEJHK');
-</script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-SK1N7CEJHK"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-SK1N7CEJHK');
+  </script>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
