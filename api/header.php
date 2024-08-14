bafa745ac60e742337ec578460de868be31edded
leon17542 <leonsoft.euler@gmail.com>
2024-08-14 19:45:32 +0530

    Add files via upload

---
+++ api/header.php
@@ -0,0 +1,7 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SK1N7CEJHK"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-SK1N7CEJHK')
+</script>
